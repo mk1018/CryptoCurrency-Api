c15e951f459d141d804245c83bc815e4f2ebedd0
mk1018 <ktmn1018@yahoo.co.jp>
2021-08-29 22:54:00 +0900

    key整理

---
+++ backend/config/key.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'etherscan' => [
+        'name' => env('ETHERSCAN_API_NAME'),
+        'key'  => env('ETHERSCAN_API_KEY'),
+    ],
+
+    'blockchain_com' => [
+        'name' => env('BLOCKCHAIN_COM_API_NAME'),
+        'key'  => env('BLOCKCHAIN_COM_API_KEY'),
+    ]
+
+];
