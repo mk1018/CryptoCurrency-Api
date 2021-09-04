540bcd257ec14e4a4b27ae89e0b0454ff7529189
mk1018 <ktmn1018@yahoo.co.jp>
2021-09-05 01:02:16 +0900

    アプリ名修正

--- backend/config/app.php
+++ backend/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Addresshow'),
+    'name' => env('APP_NAME', 'Cryptolest'),
 
     /*
     |--------------------------------------------------------------------------
--- backend/resources/views/layouts/public.blade.php
+++ backend/resources/views/layouts/public.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Addresshow') }}</title>
+    <title>{{ config('app.name', 'Cryptolest') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Addresshow') }}
+                    {{ config('app.name', 'Cryptolest') }}
                 </a>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
